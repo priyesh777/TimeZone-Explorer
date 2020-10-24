7f616f1776f080e6819648c12ad1502e02c760f1
Priyesh-Gautam <priyeshgautam1010@gmail.com>
2020-10-24 17:35:22 +0545

    finalizing the task

--- src/Zone.jsx
+++ src/Zone.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Typography, Row, Card, Button } from "antd";
+import { Typography, Row, Card, Button, Col } from "antd";
 import Select from "react-dropdown-select";
 import { EnvironmentOutlined, GlobalOutlined } from "@ant-design/icons";
 
 const Zone = () => {
   const { Text } = Typography;
+
+  const regions = [
+    { label: "Asia/Kathmandu", value: 7 },
+    { label: "America/Chicago", value: 23 },
+    { label: "America/Toronto", value: 21 },
+    { label: "Europe/Berlin", value: 2 },
+    { label: "Europe/London", value: 12 },
+    { label: "Europe/Sofia", value: 13 },
+    { label: "America/Sao_Paulo", value: 16 },
+    { label: "America/Creston", value: 17 },
+    { label: "Australia/Darwin  ", value: 77 }
+  ];
 
   const [dateStore, setDateStore] = useState({
     currentTime: "",
@@ -14,19 +26,6 @@ const Zone = () => {
   const [countryData, setCountryData] = useState([]);
   const [weather, setWeather] = useState([]);
   const [inputValue, setInputValue] = useState("");
-
-  // const [searchResult, setSearchResult] = useState({});
-
-  const regions = [
-    { label: "America/Chicago", value: 23 },
-    { label: "America/Denver", value: 21 },
-    { label: "Europe/Berlin", value: 2 },
-    { label: "Asia/Dubai", value: 29 },
-    { label: "Europe/London", value: 12 },
-    { label: "Europe/Busigen", value: 13 },
-    { label: "America/Sao_Paulo", value: 16 },
-    { label: "America/Fortalenza  ", value: 17 }
-  ];
 
   useEffect(() => {
     getCurrentTime();
@@ -43,25 +42,39 @@ const Zone = () => {
     setDateStore({ currentTime: hourMinute, currentDate: currentDateNow });
   };
 
-  const fetchData = () => {
-    //fetching names of country below
+  const fetchWeather = name => {
     fetch(
-      `https://api.timezonedb.com/v2.1/get-time-zone?key=J9X3EOT2EM8U&format=json&by=zone&zone=${inputValue}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=50bd5aef9e30aad379f3f80016fb973d`
     )
       .then(res => res.json())
       .then(json => {
+        setWeather([...weather, json]);
+        getCurrentTime();
+      });
+  };
+
+  const fetchTime = () => {
+    fetch(
+      `http://api.timezonedb.com/v2.1/get-time-zone?key=8TN2JC2CTI9L&format=json&by=zone&zone=${inputValue}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        fetchWeather(json.countryName);
         setCountryData([...countryData, json]);
         getCurrentTime();
       });
   };
-  console.log("data :::", countryData);
 
   const handleChange = values => {
     setInputValue(values.map(item => item.label));
   };
 
   const handleAdd = () => {
-    fetchData();
+    fetchTime();
+  };
+
+  const handleClear = () => {
+    window.location.reload();
   };
 
   return (
@@ -89,6 +102,15 @@ const Zone = () => {
                 style={{ marginTop: "12px", marginLeft: "2%" }}
               >
                 Add{" "}
+              </Button>
+
+              <Button
+                type="dashed"
+                onClick={handleClear}
+                danger
+                style={{ marginTop: "12px", marginLeft: "5px" }}
+              >
+                CLEAR
               </Button>
             </div>
           </div>
@@ -148,80 +170,126 @@ const Zone = () => {
             Time-zone and weather
           </p>
 
-          {countryData.map((each, index) => (
-            <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-              <Card
-                id={`country-${index}`}
-                style={{
-                  width: 440,
-                  border: "2px solid #c4c4c4",
-                  borderRadius: "10px",
-                  marginTop: "10px"
-                }}
-              >
-                {each.countryName ? (
-                  <>
-                    <div>
-                      <p style={{ fontFamily: "monospace" }}>
-                        <h1>
-                          <EnvironmentOutlined style={{ color: "#136cdc" }} />{" "}
-                          {each.countryName}
-                        </h1>
+          <Row>
+            <Col>
+              {countryData.map((each, index) => (
+                <Card
+                  id={`country-${index}`}
+                  style={{
+                    width: 440,
+                    border: "2px solid #c4c4c4",
+                    borderRadius: "10px",
+                    marginTop: "10px"
+                  }}
+                >
+                  {each.countryName ? (
+                    <>
+                      <div>
+                        <p style={{ fontFamily: "monospace" }}>
+                          <h1>
+                            <EnvironmentOutlined style={{ color: "#136cdc" }} />{" "}
+                            {each.countryName}
+                          </h1>
+                        </p>
+                        <p>
+                          <h2
+                            style={{
+                              fontFamily: "monospace",
+                              color: "#136cdc",
+                              fontWeight: "bold"
+                            }}
+                          >
+                            TimeZone : {each.formatted}
+                          </h2>
+                        </p>
+                      </div>
+                      <p
+                        style={{
+                          fontFamily: "monospace",
+                          color: "#136cdc",
+                          fontWeight: "bold"
+                        }}
+                      >
+                        Area: {each.zoneName}
                       </p>
-                      <p>
-                        <h2
+                      <p style={{ fontFamily: "monospace" }}>
+                        country code: {each.countryCode}
+                      </p>
+                    </>
+                  ) : (
+                    "No data found"
+                  )}
+                </Card>
+              ))}
+            </Col>
+
+            <Col>
+              {weather.map((each, index) => (
+                <div style={{ marginTop: "3%" }}>
+                  <Card
+                    id={`weather-${index}`}
+                    style={{
+                      width: 300,
+                      height: 227,
+                      marginLeft: "20px",
+                      border: "2px solid #c4c4c4",
+                      borderRadius: "10px"
+                    }}
+                  >
+                    <>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-evenly"
+                        }}
+                      >
+                        <GlobalOutlined
+                          style={{ color: "#136cdc", fontSize: "40px" }}
+                        />
+                        <p
                           style={{
                             fontFamily: "monospace",
                             color: "#136cdc",
-                            fontWeight: "bold"
+                            fontWeight: "bold",
+                            fontSize: "25px"
                           }}
                         >
-                          TimeZone : {each.formatted}
-                        </h2>
+                          {each.main.temp} F
+                        </p>
+                      </div>
+                      <p
+                        style={{
+                          fontFamily: "monospace",
+                          color: "#136cdc",
+                          fontWeight: "bold"
+                        }}
+                      >
+                        Humidity : {each.main.humidity} %
                       </p>
-                    </div>
-                    <p
-                      style={{
-                        fontFamily: "monospace",
-                        color: "#136cdc",
-                        fontWeight: "bold"
-                      }}
-                    >
-                      Area: {each.zoneName}
-                    </p>
-                    <p style={{ fontFamily: "monospace" }}>
-                      country code: {each.countryCode}
-                    </p>
-                  </>
-                ) : (
-                  "No data found"
-                )}
-              </Card>
-              <div>
-                <Card
-                  style={{
-                    width: 300,
-                    marginTop: "10px",
-                    marginLeft: "20px",
-                    border: "2px solid #c4c4c4",
-                    borderRadius: "10px",
-                    height: 136
-                  }}
-                >
-                  <GlobalOutlined
-                    style={{ color: "#136cdc", fontSize: "40px" }}
-                  />
-                </Card>
-                <Button
-                  type="dashed"
-                  danger
-                  style={{ float: "right", marginTop: "10px" }}
-                >
-                  delete
-                </Button>
-              </div>
-            </div>
-          ))}
+                      <p
+                        style={{
+                          fontFamily: "monospace",
+                          color: "#136cdc",
+                          fontWeight: "bold"
+                        }}
+                      >
+                        At.Pressure : {each.main.pressure} mbar
+                      </p>
+                      <p
+                        style={{
+                          fontFamily: "monospace",
+                          color: "#136cdc",
+                          fontWeight: "bold"
+                        }}
+                      >
+                        Wind : {each.wind.speed} km/hr
+                      </p>
+                    </>
+                  </Card>
+                </div>
+              ))}
+            </Col>
+          </Row>
         </div>
       </Row>
     </>
